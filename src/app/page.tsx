59288eb0a134b4e2ca0393ef4568e5e4c27742a5
minagishl <mvxd568.co@icloud.com>
2024-05-13 15:24:54 +0900

    Add tabs to Editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import React from 'react';
-import Editor from '@monaco-editor/react';
 import { editor } from 'monaco-editor';
+import Editor, { loader } from '@monaco-editor/react';
+
+// Shadcn UI
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const options: editor.IEditorOptions = {
   readOnly: false,
@@ -10,11 +12,36 @@ const options: editor.IEditorOptions = {
 };
 
 export default function Home() {
+  loader.init().then((monaco) => {
+    monaco.editor.defineTheme('custom', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [],
+      colors: {
+        'editor.background': '#09090B',
+      },
+    });
+  });
+
   return (
     <main className="flex min-h-screen">
       <div className="flex-1 bg-gray-200">Left Content</div>
-      <div className="flex-1">
-        <Editor defaultLanguage="css" theme="vs-dark" options={options} />
+      <div className="flex-1 dark">
+        <div className="bg-background">
+          <Tabs defaultValue="css" className="w-fit p-2">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="css">index.css</TabsTrigger>
+              <TabsTrigger value="view">view</TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
+        <Editor
+          defaultLanguage="css"
+          theme="custom"
+          options={options}
+          height="100dvh"
+          width="100%"
+        />
       </div>
     </main>
   );
