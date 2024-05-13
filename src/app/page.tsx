47b6d740118cb495b5e9c9da2befb0348e3d77a9
minagishl <mvxd568.co@icloud.com>
2024-05-13 15:52:49 +0900

    Changed to be able to resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { useEffect } from 'react';
 import { editor } from 'monaco-editor';
 import Editor, { loader } from '@monaco-editor/react';
 
 // Shadcn UI
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
 
 const options: editor.IEditorOptions = {
   readOnly: false,
@@ -12,37 +18,48 @@ const options: editor.IEditorOptions = {
 };
 
 export default function Home() {
-  loader.init().then((monaco) => {
-    monaco.editor.defineTheme('custom', {
-      base: 'vs-dark',
-      inherit: true,
-      rules: [],
-      colors: {
-        'editor.background': '#09090B',
-      },
+  const minSize = 30;
+
+  useEffect(() => {
+    loader.init().then((monaco) => {
+      monaco.editor.defineTheme('custom', {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [],
+        colors: {
+          'editor.background': '#09090B',
+        },
+      });
     });
-  });
+  }, []);
 
   return (
     <main className="flex min-h-screen">
-      <div className="flex-1 bg-gray-200">Left Content</div>
-      <div className="flex-1 dark flex flex-col">
-        <div className="bg-background">
-          <Tabs defaultValue="css" className="w-fit p-2">
-            <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="css">index.css</TabsTrigger>
-              <TabsTrigger value="view">view</TabsTrigger>
-            </TabsList>
-          </Tabs>
-        </div>
-        <Editor
-          defaultLanguage="css"
-          theme="custom"
-          options={options}
-          height="100dvh"
-          width="100%"
-        />
-      </div>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel minSize={minSize}>
+          <div className="flex-1 bg-gray-200">Left Content</div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel minSize={minSize}>
+          <div className="flex-1 dark flex flex-col">
+            <div className="bg-background">
+              <Tabs defaultValue="css" className="w-fit p-2">
+                <TabsList className="grid w-full grid-cols-2">
+                  <TabsTrigger value="css">index.css</TabsTrigger>
+                  <TabsTrigger value="view">view</TabsTrigger>
+                </TabsList>
+              </Tabs>
+            </div>
+            <Editor
+              defaultLanguage="css"
+              theme="custom"
+              options={options}
+              height="100dvh"
+              width="100%"
+            />
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   );
 }
